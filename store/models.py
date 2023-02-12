2cafff00a8eaa524d1ebb1e636464e805b7e0842
Duy <duy.leduc8@gmail.com>
2023-02-12 17:15:46 +0700

    update product model, make migrations

--- store/models.py
+++ store/models.py
@@ -14,8 +14,9 @@ class Collection(models.Model):
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True) # ? Django fill everytime record got updated
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
