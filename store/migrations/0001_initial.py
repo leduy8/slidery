f4f272663a27bdadebcf63d13c004116b1577979
Duy <duy.leduc8@gmail.com>
2023-02-14 11:26:02 +0700

    update migrations

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -42,7 +42,7 @@ class Migration(migrations.Migration):
             name='Order',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('place_at', models.DateTimeField(auto_now_add=True)),
+                ('placed_at', models.DateTimeField(auto_now_add=True)),
                 ('payment_status', models.CharField(choices=[('P', 'Pending'), ('C', 'Complete'), ('F', 'Failed')], default='P', max_length=1)),
                 ('customer', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='store.customer')),
             ],
