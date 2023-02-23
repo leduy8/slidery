ef98776807facfe16b1779d6e60f56c700ea2b94
Duy <duy.leduc8@gmail.com>
2023-02-23 16:16:13 +0700

    JOIN, AGGREGATE, ANNOTATE

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 
-from store.models import Product
+from store.models import Product, Order, Customer
 
 
 def say_hello(request):
     query_set = []
     product = None
+    result = None
 
     # FILTER
-    query_set = Product.objects.filter(unit_price__range=(20, 30)).all()
+    # query_set = Product.objects.filter(unit_price__range=(20, 30)).all()
     # query_set = Product.objects.filter(collection__id__range=(1, 3)).all()
     # query_set = Product.objects.filter(title__icontains='coffee').all()
     # query_set = Product.objects.filter(last_update__year=2021).all()
@@ -41,4 +43,30 @@ def say_hello(request):
     # Specific field to query
     # query_set = Product.objects.values('id', 'title', 'collection__title')
 
-    return render(request, 'hello.html', {'name': 'Duy', 'products': list(query_set), 'product': product})
+    # JOIN
+    # select_related (1)
+    # query_set = Product.objects.select_related('collection').all()
+    # prefetch_related (n)
+    # query_set = Product.objects.prefetch_related('promotions').all()
+    # Nested JOIN
+    # query_set = Product.objects.prefetch_related('promotions').select_related('collection').all()
+
+    # Exercise: Get the last 5 orders with their customer and items (including product)
+    # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # #? Access data after query
+    # for query in query_set:
+    #     #? Access Order data
+    #     print(query.customer.first_name)
+    #     for ord_item in query.orderitem_set.all():
+    #         #? Access Order Item data
+    #         print(ord_item.product.title)
+
+    # AGGRIGATE
+    # result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
+    # result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+
+    # ANNOTATING
+    # query_set = Customer.objects.annotate(is_new=True).all()
+
+
+    return render(request, 'hello.html', {'name': 'Duy', 'products': list(query_set), 'product': product, 'result': result})
