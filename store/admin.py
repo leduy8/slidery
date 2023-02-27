584b3403fc2db054c9091fd0a02b0bcc0a48cee4
Duy <duy.leduc8@gmail.com>
2023-02-27 16:18:21 +0700

    override base queryset

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models.aggregates import Count
 from . import models
 
 @admin.register(models.Product)
@@ -31,5 +33,15 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
     list_per_page = 10
 
-# Register your models here.
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request: HttpRequest):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
