e3e6c8df1ae86097b557a9f70708197bb04dc7fd
Duy <duy.leduc8@gmail.com>
2023-02-27 14:47:56 +0700

    setup models for admin panel

--- store/admin.py
+++ store/admin.py
@@ -3,15 +3,19 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'Low'
         return 'Okay'
+
+    def collection_title(self, product):
+        return product.collection.title
 
 
 @admin.register(models.Customer)
@@ -20,6 +24,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
 
 # Register your models here.
 admin.site.register(models.Collection)
