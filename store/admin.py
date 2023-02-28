3e918b351a6bd5c6fbbc442a2a9f9bedbcf3dea4
Duy <duy.leduc8@gmail.com>
2023-02-28 08:55:43 +0700

    add search, add filter, add custom filter

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,5 @@
+from typing import Any, List, Tuple
+
 from django.contrib import admin
 from django.http import HttpRequest
 from django.db.models.aggregates import Count
@@ -5,12 +7,28 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from . import models
 
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request: Any, model_admin: Any) -> List[Tuple[Any, str]]:
+        return [
+            ('<10', 'Low'),
+        ]
+
+    def queryset(self, request: Any, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -28,6 +46,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders(self, customer):
