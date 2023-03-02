b1f3619b07ac762a7e6d6843057ef89347476afd
Duy <duy.leduc8@gmail.com>
2023-03-02 10:49:00 +0700

    Deserializer

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,11 +8,19 @@ from .serializer import ProductSerializer
 
 
 # Create your views here.
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True, context={"request": request})
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data
+        return Response("ok")
 
 
 @api_view()
