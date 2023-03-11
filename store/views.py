a970d51aa119daa3806a215167289c39c58a46f4
Duy <duy.leduc8@gmail.com>
2023-03-11 20:09:53 +0700

    create Cart API

--- store/views.py
+++ store/views.py
@@ -2,13 +2,19 @@ from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from .filters import ProductFilter
-from .models import Collection, OrderItem, Product, Review
+from .models import Cart, Collection, OrderItem, Product, Review
 from .pagination import DefaultPagination
-from .serializer import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .serializer import (
+    CartSerializer,
+    CollectionSerializer,
+    ProductSerializer,
+    ReviewSerializer,
+)
 
 
 class ProductViewSet(ModelViewSet):
@@ -59,3 +65,8 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {"product_id": self.kwargs["product_pk"]}
+
+
+class CartViewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
