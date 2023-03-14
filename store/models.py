816d3579eafc291660f7747b3077781b214d6d8b
Duy <duy.leduc8@gmail.com>
2023-03-14 11:19:42 +0700

    group and permission

--- store/models.py
+++ store/models.py
@@ -98,6 +98,9 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [("cancel_order", "Can change order")]
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
