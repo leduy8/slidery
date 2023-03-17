dd45a5356e8e8cc5583a10859a598ce9e6befbb9
Duy <duy.leduc8@gmail.com>
2023-03-17 13:16:36 +0700

    setup media files

--- storefront/urls.py
+++ storefront/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path("auth/", include("djoser.urls.jwt")),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
