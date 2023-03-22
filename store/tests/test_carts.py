f866a2176969446ada4f69b05090f74ff2c48f74
Duy <duy.leduc8@gmail.com>
2023-03-22 21:01:28 +0700

    add test for cart

---
+++ store/tests/test_carts.py
@@ -0,0 +1,16 @@
+from uuid import uuid4
+from datetime import datetime
+
+from django.test import TestCase
+from store.models import Cart
+
+
+class CartTestCase(TestCase):
+    my_uuid = uuid4()
+
+    def setUp(self) -> None:
+        Cart.objects.create(id=self.my_uuid, created_at=datetime.now())
+
+    def test_cart_creation(self):
+        cart = Cart.objects.get(id=self.my_uuid)
+        self.assertIsInstance(cart, Cart)
